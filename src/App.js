14493d7e80b3cad3eb7093b45b63afa851685ec2
Md. Mirajul Islam Tashfi <mirajul.tashfi12@gmail.com>
2025-01-13 22:26:15 +0600

    Updating Components

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ShopCategory from './Pages/ShopCategory';
 import Product from './Pages/Product';
 import LoginSignup from './Pages/LoginSignup';
 import Cart from './Pages/Cart';
+import Footer from './Components/Footer/Footer';
  
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         <Route path='/login' element={<LoginSignup/>}/>
         <Route path='/cart' element={<Cart/>}/>
       </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
