a22a2403e0b35f59219e0fb68437f6167712eff0
Md. Mirajul Islam Tashfi <mirajul.tashfi12@gmail.com>
2025-01-14 01:14:39 +0600

    products link

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { ShopContext } from '../Context/ShopContext';
+import { useParams } from 'react-router-dom';
+import Breadcrum from '../Components/Breadcrums/Breadcrum';
 
 const Product = () => {
+  const { all_product } = useContext(ShopContext); // Get products from context
+  const { productId } = useParams(); // Get productId from URL
+  const product = all_product.find((e) => e.id === Number(productId)); // Find the product by id
+
   return (
     <div>
-        
+      <Breadcrum product={product} /> {/* Pass the product */}
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
